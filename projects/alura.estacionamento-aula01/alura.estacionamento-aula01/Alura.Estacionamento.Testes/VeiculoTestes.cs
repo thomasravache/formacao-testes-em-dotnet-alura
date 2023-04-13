408e16989df84ab29d47521669205356ea6ceac6
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2023-04-13 10:10:12 -0300

    feat: Testes unitários e o padrão AAA

--- projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -9,18 +9,26 @@ namespace Alura.Estacionamento.Testes
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            // Arrange
             var veiculo = new Veiculo();
+
+            // Act
             veiculo.Acelerar(10);
 
+            // Assert
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
         [Fact]
         public void TestaVeiculoFrear()
         {
+            // Arrange
             var veiculo = new Veiculo();
+
+            // Act
             veiculo.Frear(10);
 
+            // Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
     }
