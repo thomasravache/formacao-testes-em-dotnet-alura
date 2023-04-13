ac5228814ad610e80e3105d12838bf83d798d448
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2023-04-13 09:40:24 -0300

    feat: Criando um projeto com testes

---
+++ projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,27 @@
+using System;
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Acelerar(10);
+
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
