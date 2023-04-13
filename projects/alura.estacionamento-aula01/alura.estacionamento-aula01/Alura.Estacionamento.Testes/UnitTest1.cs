2ca2205c0f36e896cec724b3b64621b56e8b90ab
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2023-04-13 08:35:01 -0300

    chore: adicionar projeto de testes ao .sln do projeto

---
+++ projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
