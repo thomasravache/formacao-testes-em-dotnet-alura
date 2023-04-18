2e24526b75589b10e61fda30a4424b9f5b8ae50c
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2023-04-17 23:11:53 -0300

    feat: Utilizando xUnit

---
+++ projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,32 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = "Thomas";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            // Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
