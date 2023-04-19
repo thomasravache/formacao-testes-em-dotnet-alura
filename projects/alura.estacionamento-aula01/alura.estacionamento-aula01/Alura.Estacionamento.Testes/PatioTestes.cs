501cf4baf0d9387ccc106a8dcd73979be133adee
Thomas Ravache <thomasravache31@gmail.com>
2023-04-19 01:09:43 -0300

    feat: Anotações do xUnit

--- projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/PatioTestes.cs
+++ projects/alura.estacionamento-aula01/alura.estacionamento-aula01/Alura.Estacionamento.Testes/PatioTestes.cs
@@ -4,29 +4,64 @@ using Xunit;
 
 namespace Alura.Estacionamento.Testes
 {
-    public class PatioTestes
+  public class PatioTestes
+  {
+    [Fact]
+    public void ValidaFaturamento()
     {
-        [Fact]
-        public void ValidaFaturamento()
-        {
-            // Arrange
-            var estacionamento = new Patio();
-            var veiculo = new Veiculo();
+      // Arrange
+      var estacionamento = new Patio();
+      var veiculo = new Veiculo();
 
-            veiculo.Proprietario = "Thomas";
-            veiculo.Tipo = TipoVeiculo.Automovel;
-            veiculo.Cor = "Verde";
-            veiculo.Modelo = "Fusca";
-            veiculo.Placa = "asd-9999";
+      veiculo.Proprietario = "Thomas";
+      veiculo.Tipo = TipoVeiculo.Automovel;
+      veiculo.Cor = "Verde";
+      veiculo.Modelo = "Fusca";
+      veiculo.Placa = "asd-9999";
 
-            estacionamento.RegistrarEntradaVeiculo(veiculo);
-            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+      estacionamento.RegistrarEntradaVeiculo(veiculo);
+      estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
 
-            // Act
-            double faturamento = estacionamento.TotalFaturado();
+      // Act
+      double faturamento = estacionamento.TotalFaturado();
 
-            // Assert
-            Assert.Equal(2, faturamento);
-        }
+      // Assert
+      Assert.Equal(2, faturamento);
     }
+
+    /*
+        Permite que o teste seja feito com vários tipos de entrada de valor
+        não deixando o teste "viciado", fazendo a validação de mais cenários
+
+        Espero que em todos os casos abaixo o resultado seja o mesmo, mesmo passando valores
+        diferentes
+    */
+    [Theory] // permite trabalhar com um conjunto maior de dados
+    [InlineData("André Silva", "ASD-1498", "preto", "Gol")]
+    [InlineData("Jose Silva", "POL-9242", "Cinza", "Fusca")]
+    [InlineData("Maria Silva", "GDR-6524", "Azul", "Opala")]
+    [InlineData("Pedro Silva", "GDR-0101", "Azul", "Corsa")]
+    public void ValidaFaturamentoComVariosVeiculos(
+        string proprietario,
+        string placa,
+        string cor,
+        string modelo
+    )
+    {
+      // Arrange
+      Patio estacionamento = new Patio();
+      var veiculo = new Veiculo();
+      veiculo.Proprietario = proprietario;
+      veiculo.Placa = placa;
+      veiculo.Cor = cor;
+      veiculo.Modelo = modelo;
+
+      estacionamento.RegistrarEntradaVeiculo(veiculo);
+      estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+      // Act
+      double faturamento = estacionamento.TotalFaturado();
+      // Assert
+      Assert.Equal(2, faturamento);
+    }
+  }
 }
